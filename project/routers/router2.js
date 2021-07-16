bb9aee61a6e0cf4e6911485ea556194853d205cd
Giannhs Diamantopoulos <74733098+giandiam@users.noreply.github.com>
2021-07-16 22:29:11 +0300

    Add files via upload

---
+++ project/routers/router2.js
@@ -0,0 +1,106 @@
+const express = require('express');
+
+const router = express.Router();
+
+const db = require('../db/db');
+
+/* MyAlbums */
+
+router.get('/app', async (req, res) => {
+  const query1 = {
+    text: 'SELECT * FROM "albums" WHERE "user" = $1',
+    values: req.session.user,
+  };
+  try {
+    const rows1 = await db.query(query1);
+    res.render('app', {
+      items1: rows1, myalbums: true, myartists: false, mysongs: false,
+    });
+  } catch (err) { console.log(err.stack); }
+});
+
+router.post('/app', async (req, res) => {
+  const query1 = {
+    text: 'SELECT * FROM "albums" WHERE "user" = $1',
+    values: req.session.user,
+  };
+  const query2 = {
+    text: 'SELECT * FROM "albums" WHERE "user" = $1 and name = $2',
+    values: [req.session.user, req.body.selectpicker],
+  };
+  try {
+    const rows1 = await db.query(query1);
+    const rows2 = await db.query(query2);
+    res.render('app', {
+      items1: rows1, items2: rows2, myalbums: true, myartists: false, mysongs: false,
+    });
+  } catch (err) { console.log(err.stack); }
+});
+
+/* MyArtists */
+
+router.get('/app/myartists', async (req, res) => {
+  const query1 = {
+    text: 'SELECT * FROM "artists" WHERE "user" = $1',
+    values: req.session.user,
+  };
+  try {
+    const rows1 = await db.query(query1);
+    res.render('app', {
+      items1: rows1, myartists: true, myalbums: false, mysongs: false,
+    });
+  } catch (err) { console.log(err.stack); }
+});
+
+router.post('/app/myartists', async (req, res) => {
+  const query1 = {
+    text: 'SELECT * FROM "artists" WHERE "user" = $1',
+    values: req.session.user,
+  };
+  const query2 = {
+    text: 'SELECT * FROM "artists" WHERE "user" = $1 and artist = $2',
+    values: [req.session.user, req.body.selectpicker],
+  };
+  try {
+    const rows1 = await db.query(query1);
+    const rows2 = await db.query(query2);
+    res.render('app', {
+      items1: rows1, items2: rows2, myalbums: false, myartists: true, mysongs: false,
+    });
+  } catch (err) { console.log(err.stack); }
+});
+
+/* MySongs */
+
+router.get('/app/mysongs', async (req, res) => {
+  const query1 = {
+    text: 'SELECT * FROM "mysongs" WHERE "user" = $1',
+    values: req.session.user,
+  };
+  try {
+    const rows1 = await db.query(query1);
+    res.render('app', {
+      items1: rows1, myartists: false, myalbums: false, mysongs: true,
+    });
+  } catch (err) { console.log(err.stack); }
+});
+
+router.post('/app/mysongs', async (req, res) => {
+  const query1 = {
+    text: 'SELECT * FROM "mysongs" WHERE "user" = $1',
+    values: req.session.user,
+  };
+  const query2 = {
+    text: 'SELECT * FROM "mysongs" WHERE "user" = $1 and title = $2',
+    values: [req.session.user, req.body.selectpicker],
+  };
+  try {
+    const rows1 = await db.query(query1);
+    const rows2 = await db.query(query2);
+    res.render('app', {
+      items1: rows1, items2: rows2, myalbums: false, myartists: false, mysongs: true,
+    });
+  } catch (err) { console.log(err.stack); }
+});
+
+module.exports = router;
