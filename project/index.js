29f6249ed76a78b454470461abc73edb2bec14fe
Giannhs Diamantopoulos <74733098+giandiam@users.noreply.github.com>
2021-07-20 15:24:32 +0300

    Update index.js

--- project/index.js
+++ project/index.js
@@ -1,43 +1,53 @@
-const express = require('express'); /** assign 'express'
-module into express */
-const app = express(); /** assign into app the express() function that exported
-by express module above */
+const express = require('express');
+const app = express();
 const session = require('express-session');
 const bodyparser = require('body-parser');
 const router1 = require('./routers/router1');
 const router2 = require('./routers/router2');
 const router3 = require('./routers/router3');
+const router4 = require('./routers/router4');
+const router5 = require('./routers/router5');
 
 app.use(bodyparser.urlencoded({ extended: true }));
 
-app.set('view engine', 'ejs'); /**  */
+app.set('view engine', 'ejs');
 
 app.use(session({
   secret: 'keyboard cat',
   resave: false,
-  saveUninitialized: true,
+  saveUninitialized: false,
 }));
-
-/* route for serving frontend files */
 
 app.use('/', router1);
 app.use('/', router2);
 app.use('/', router3);
+app.use('/', router4);
+app.use('/', router5);
+
+app.use((req, res, next) => {
+  res.setHeader('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-age=0, post-check=0, pre-check=0');
+  next();
+});
 
 app.get('/', (req, res) => {
+  if (req.session.user) {
+    res.redirect('/app');
+  }
   res.render('index');
 });
 
 app.get('/login', (req, res) => {
+  if (req.session.user) {
+    res.redirect('/app');
+  }
   res.render('index');
 });
 
 app.get('/signup', (req, res) => {
+  if (req.session.user) {
+    res.redirect('/app');
+  }
   res.render('sign-up');
-});
-
-app.get('/app', (req, res) => {
-  res.render('app');
 });
 
 app.use(express.static('public'));
